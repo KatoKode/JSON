561245407f2aa1fe252d1f928681248f62409fe3
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-06-26 12:13:26 -0400

    Add files via upload

---
+++ json/json.c
@@ -0,0 +1,9 @@
+#include "json.h"
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// LIBRARY CONSTRUCTOR
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void __attribute__ ((constructor)) initJSONLibrary(void) { }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// LIBRARY DESTRUCTOR
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void __attribute__ ((destructor)) termJSONLibrary(void) { }
